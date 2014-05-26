9f24c63505ddcf3ad5dc384577078059ccb4624e
siyafrica <siyafrica@gmail.com>
2014-05-26 08:55:38 +0200

    Reconfigureed the maps layout

--- js/map1.js
+++ js/map1.js
@@ -1,18 +1,22 @@
-    var width = 300;
-    var height = 400;
+            var width = 460;
+            var height = 400;
             
-    //new projection
-    //new projection
+            var color = d3.scale.threshold()
+                            .domain([10, 20, 30, 40, 50])
+                            .range(["#FF9B0D", "#E86D0C", "#FF4B00", "#E82D0C", "#FF0E0D"]);
+              
+            //TopoJSON Map
+            //new projection
 	    var projection = d3.geo.mercator()
                                 .center([36.8000, 1.2667])
 				.scale([1000])
                                 .translate([width/2, height/2]);
                         
                         
-    var path = d3.geo.path().projection(projection);
+            var path = d3.geo.path().projection(projection);
     
     
-    var svg = d3.select("div#Map1")
+             var svg = d3.select("div#Map1")
                         .append("svg")
                         .attr("width", width)
                         .attr("height", height)
@@ -23,14 +27,16 @@
                               .scaleExtent([1, 10])
                               .on("zoom", zoom));
                         
-            d3.json("js/kenya-topo.json", function(error, topology) {
+            d3.json("js/output.json", function(error, topology) {
                 g.selectAll("path")
-                .data(topojson.feature(topology, topology.objects.Kenya).features)
+                .data(topojson.feature(topology, topology.objects.kenya).features)
                 .enter()
                 .append("path")
                 .attr("d", path);
-                });
+            
+            svg.append(
+                       ("rect").attr("width", width).attr("height", height).style("stroke", "black").style("fill", "none")
+                       );
             
             function zoom() {
-                g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-            };
+                g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")")}
